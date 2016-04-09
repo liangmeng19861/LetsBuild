4b2f844b933757bc466bc94a49f191c3d343ae07
lilg <lilg@asiainfo.com>
2016-04-10 02:56:01 +0800

    1

--- src/main/java/com/letsbuild/controller/ManageController.java
+++ src/main/java/com/letsbuild/controller/ManageController.java
@@ -1,5 +1,6 @@
 package com.letsbuild.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,8 +13,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.alibaba.fastjson.JSON;
-import com.letsbuild.dao.mapper.bo.User;
-import com.letsbuild.service.interfaces.IUserSV;
+import com.letsbuild.dao.mapper.bo.SysMenu;
+import com.letsbuild.dao.mapper.bo.SysUser;
+import com.letsbuild.service.interfaces.ISysMenuSV;
+import com.letsbuild.service.interfaces.ISysUserSV;
 
 @Controller
 @RequestMapping(value = "/manage")
@@ -22,14 +25,22 @@ public class ManageController extends BaseController {
 	private static final Logger logger = LogManager.getLogger(ManageController.class);
 	
 	@Autowired
-	private IUserSV iUserSV;
+	private ISysUserSV iUserSV;
+	@Autowired
+	private ISysMenuSV iSysMenuSV;
 	
 	@RequestMapping(value = "/userlist")
 	public String queryuser(HttpServletRequest request, HttpServletResponse response){
 		logger.info("开始用户查询");
-		List<User> userList = iUserSV.queryUser();
+		List<SysUser> userList = iUserSV.querySysUser();
 		logger.info(JSON.toJSONString(userList));
 		logger.info("用户查询结束");
 		return "/index";
+	}
+	
+	@RequestMapping(value="/menulist")
+	public void queryMenu(HttpServletRequest request, HttpServletResponse response){
+		List<SysMenu> menuList = iSysMenuSV.querySysMenu(new ArrayList<Long>());
+		responseSuccess(response,"菜单加载完毕",JSON.toJSONString(menuList));
 	}
 }
--- src/main/java/com/letsbuild/dao/mapper/factory/MapperFactory.java
+++ src/main/java/com/letsbuild/dao/mapper/factory/MapperFactory.java
@@ -6,29 +6,72 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.letsbuild.dao.mapper.interfaces.UserMapper;
-
+import com.letsbuild.dao.mapper.interfaces.OrdAdvpayMapper;
+import com.letsbuild.dao.mapper.interfaces.OrdForegiftMapper;
+import com.letsbuild.dao.mapper.interfaces.OrdOrderMapper;
+import com.letsbuild.dao.mapper.interfaces.OrdStageMapper;
+import com.letsbuild.dao.mapper.interfaces.SysLabelMapper;
+import com.letsbuild.dao.mapper.interfaces.SysMenuMapper;
+import com.letsbuild.dao.mapper.interfaces.SysRoleMapper;
+import com.letsbuild.dao.mapper.interfaces.SysRoleMenuMapper;
+import com.letsbuild.dao.mapper.interfaces.SysUserMapper;
+import com.letsbuild.dao.mapper.interfaces.SysUserRoleMapper;
 
 @Component
 public class MapperFactory {
 
-    @Autowired
-    private SqlSessionTemplate st;
+	@Autowired
+	private SqlSessionTemplate st;
 
-    private static SqlSessionTemplate sqlSessionTemplate;
+	private static SqlSessionTemplate sqlSessionTemplate;
 
-    @PostConstruct
-    void init() {
-        setSqlSessionTemplate(st);
-    }
+	@PostConstruct
+	void init() {
+		setSqlSessionTemplate(st);
+	}
 
-    public static void setSqlSessionTemplate(SqlSessionTemplate st) {
-        MapperFactory.sqlSessionTemplate = st;
-    }
+	public static void setSqlSessionTemplate(SqlSessionTemplate st) {
+		MapperFactory.sqlSessionTemplate = st;
+	}
 
-    public static UserMapper getUserMapper() {
-        return sqlSessionTemplate.getMapper(UserMapper.class);
-    }
-    
+	public static OrdAdvpayMapper getOrdAdvpayMapper() {
+		return sqlSessionTemplate.getMapper(OrdAdvpayMapper.class);
+	}
+
+	public static OrdForegiftMapper getOrdForegiftMapper() {
+		return sqlSessionTemplate.getMapper(OrdForegiftMapper.class);
+	}
+
+	public static OrdOrderMapper getOrdOrderMapper() {
+		return sqlSessionTemplate.getMapper(OrdOrderMapper.class);
+	}
+
+	public static OrdStageMapper getOrdStageMapper() {
+		return sqlSessionTemplate.getMapper(OrdStageMapper.class);
+	}
+
+	public static SysLabelMapper getSysLabelMapper() {
+		return sqlSessionTemplate.getMapper(SysLabelMapper.class);
+	}
+
+	public static SysMenuMapper getSysMenuMapper() {
+		return sqlSessionTemplate.getMapper(SysMenuMapper.class);
+	}
+
+	public static SysRoleMapper getSysRoleMapper() {
+		return sqlSessionTemplate.getMapper(SysRoleMapper.class);
+	}
+
+	public static SysRoleMenuMapper getSysRoleMenuMapper() {
+		return sqlSessionTemplate.getMapper(SysRoleMenuMapper.class);
+	}
+
+	public static SysUserMapper getSysUserMapper() {
+		return sqlSessionTemplate.getMapper(SysUserMapper.class);
+	}
+
+	public static SysUserRoleMapper getSysUserRoleMapper() {
+		return sqlSessionTemplate.getMapper(SysUserRoleMapper.class);
+	}
 
 }
